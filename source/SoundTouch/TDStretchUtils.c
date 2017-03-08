8b468a9ff726318575d1ea5438a5c2b0d6dae4e3
edopao <edopao@users.noreply.github.com>
2017-03-08 19:26:09 +0100

    [CODE] Code refactoring in parallel region
    Two main changes:
    * Removed function call inside parallel region
    * Critical region replaced with reduction

--- source/SoundTouch/TDStretchUtils.c
+++ source/SoundTouch/TDStretchUtils.c
@@ -3,6 +3,10 @@
 #include "float.h"
 #include "math.h"
 
+typedef struct CorrResult {
+    double corr;
+    int offs;
+} CorrResult_t;
 
 double calcCrossCorr(int channels, int overlapLength, const float *mixingPos, const float *compare, double *anorm_ptr);
 double calcCrossCorrAccumulate(int channels, int overlapLength, const float *mixingPos, const float *compare, double *norm_ptr);
@@ -10,65 +14,83 @@ double calcCrossCorrAccumulate(int channels, int overlapLength, const float *mix
 
 int seekBestOverlapPositionFullImpl(int channels, int seekLength, int overlapLength, SAMPLETYPE *pMidBuffer, const SAMPLETYPE *refPos)
 {
-    int bestOffs;
-    double bestCorr;
     int i;
     double norm;
-
-    bestCorr = -FLT_MAX;
-    bestOffs = 0;
+    CorrResult_t bestCorr = { -FLT_MAX, 0 };
 
     // Scans for the best correlation value by testing each possible position
     // over the permitted range.
-    bestCorr = calcCrossCorr(channels, overlapLength, refPos, pMidBuffer, &norm);
-    bestCorr = (bestCorr + 0.1) * 0.75;
+    bestCorr.corr = calcCrossCorr(channels, overlapLength, refPos, pMidBuffer, &norm);
+    bestCorr.corr = (bestCorr.corr + 0.1) * 0.75;
 
-    #pragma omp parallel for
+    #pragma omp declare reduction (maxCorr : CorrResult_t :      \
+        omp_out = omp_in.corr > omp_out.corr ? omp_out : omp_in) \
+        initializer ( omp_priv = { 0.0, 0 } )
+
+    #pragma omp parallel for reduction (maxCorr:bestCorr)
     for (i = 1; i < seekLength; i ++)
     {
         double corr;
+
         // Calculates correlation value for the mixing position corresponding to 'i'
 #ifdef _OPENMP
         // in parallel OpenMP mode, can't use norm accumulator version as parallel executor won't
         // iterate the loop in sequential order
-        corr = calcCrossCorr(channels, overlapLength, refPos + channels * i, pMidBuffer, &norm);
+        {
+            const float *mixingPos = (const float *)(refPos + channels * i);
+            const float *compare = (const float *)pMidBuffer;
+            int i;
+
+            corr = norm = 0;
+            // Same routine for stereo and mono. For Stereo, unroll by factor of 2.
+            // For mono it's same routine yet unrolls by factor of 4.
+            for (i = 0; i < channels * overlapLength; i += 4)
+            {
+                corr += mixingPos[i] * compare[i] +
+                        mixingPos[i + 1] * compare[i + 1];
+
+                norm += mixingPos[i] * mixingPos[i] +
+                        mixingPos[i + 1] * mixingPos[i + 1];
+
+                // unroll the loop for better CPU efficiency:
+                corr += mixingPos[i + 2] * compare[i + 2] +
+                        mixingPos[i + 3] * compare[i + 3];
+
+                norm += mixingPos[i + 2] * mixingPos[i + 2] +
+                        mixingPos[i + 3] * mixingPos[i + 3];
+            }
+
+            corr = corr / sqrt((norm < 1e-9 ? 1.0 : norm));
+        }
 #else
         // In non-parallel version call "calcCrossCorrAccumulate" that is otherwise same
         // as "calcCrossCorr", but saves time by reusing & updating previously stored
         // "norm" value
         corr = calcCrossCorrAccumulate(channels, overlapLength, refPos + channels * i, pMidBuffer, &norm);
 #endif
-        // heuristic rule to slightly favour values close to mid of the range
+
+        // heuristic rule to slightly favor values close to mid of the range
         double tmp = (double)(2 * i - seekLength) / (double)seekLength;
         corr = ((corr + 0.1) * (1.0 - 0.25 * tmp * tmp));
 
+        CorrResult_t currentCorr = { corr, i };
+
         // Checks for the highest correlation value
-        if (corr > bestCorr)
+        if (currentCorr.corr > bestCorr.corr)
         {
-            // For optimal performance, enter critical section only in case that best value found.
-            // in such case repeat 'if' condition as it's possible that parallel execution may have
-            // updated the bestCorr value in the mean time
-            #pragma omp critical
-            if (corr > bestCorr)
-            {
-                bestCorr = corr;
-                bestOffs = i;
-            }
+            bestCorr.corr = currentCorr.corr;
+            bestCorr.offs = currentCorr.offs;
         }
     }
 
-#ifdef SOUNDTOUCH_INTEGER_SAMPLES
-    adaptNormalizer();
-#endif
-
-    return bestOffs;
+    return bestCorr.offs;
 }
 
 
 /// Calculate cross-correlation
 double calcCrossCorr(int channels, int overlapLength, const float *mixingPos, const float *compare, double *anorm_ptr)
 {
-	double anorm = *anorm_ptr;
+    double anorm = *anorm_ptr;
     double corr;
     double norm;
     int i;
@@ -100,7 +122,7 @@ double calcCrossCorr(int channels, int overlapLength, const float *mixingPos, co
 /// Update cross-correlation by accumulating "norm" coefficient by previously calculated value
 double calcCrossCorrAccumulate(int channels, int overlapLength, const float *mixingPos, const float *compare, double *norm_ptr)
 {
-	double norm = *norm_ptr;
+    double norm = *norm_ptr;
     double corr;
     int i, j;
 
